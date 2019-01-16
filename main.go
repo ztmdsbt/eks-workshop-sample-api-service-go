9772414f07b78199d819d9931523066e576d458e
Kaihang An <ztmdsbt@gmail.com>
2019-01-16 12:19:24 +0800

    Update main.go

--- main.go
+++ main.go
@@ -33,7 +33,7 @@ func main() {
 
 		io.WriteString(w, string(out))
 
-		fmt.Println("Hello world - the log message")
+		fmt.Println("Hello world from AKH - the log message")
 	})
 	http.ListenAndServe(":8080", nil)
 }
